0da7100cedcb1b0e91393b64ec036df5dff4148f
Hans Snah <example@hans.com>
2013-07-10 23:29:31 +0000

    new verion

--- web.js
+++ web.js
@@ -3,10 +3,13 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var fs = require('fs'); 
-
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync("index.html", "utf-8");
+
+var fs = require('fs');
+
+var buffer = new buffer ();
+
+  response.send(buffer.toString(fs.readFileSync("index.html"));
 });
 
 var port = process.env.PORT || 5000;
